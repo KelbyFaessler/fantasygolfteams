f020c40a8e2861673f6bdc68e70a3d1a212beab1
Kelby Faessler <kmfaessler@gmail.com>
2019-12-16 05:52:46 -0800

    add logging to production

--- config/settings/production.py
+++ config/settings/production.py
@@ -28,3 +28,42 @@ DATABASES = {}
 # The following line automatically uses the 'DATABASE_URL' environment variable
 # see https://pypi.org/project/dj-database-url/ for more info
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
+                       'pathname=%(pathname)s lineno=%(lineno)s '
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
